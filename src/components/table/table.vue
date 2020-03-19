cfc53957b9482a90c15febae45f48f6325b1c8eb
qietubaby <201951244@qq.com>
2020-03-19 12:33:49 +0800

    修改table组件

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -1,27 +1,21 @@
 <template>
-  <div
-    class="gulu-table-wrapper"
-    ref="wrapper"
-  >
-    <div
-      ref="tableWrapper"
-      :style="{height,overflow:'auto'}"
-    >
+  <div class="gulu-table-wrapper" ref="wrapper">
+    <div ref="tableWrapper" :style="{ height, overflow: 'auto' }">
       <table
         class="gulu-table"
-        :class="{bordered, compact, striped: striped}"
+        :class="{ bordered, compact, striped: striped }"
         ref="table"
       >
         <thead>
           <tr>
             <th
               v-if="expendField"
-              :style="{width:'50px'}"
+              :style="{ width: '50px' }"
               class="gulu-table-center"
             ></th>
             <th
               v-if="checkable"
-              :style="{width:'50px'}"
+              :style="{ width: '50px' }"
               class="gulu-table-center"
             >
               <input
@@ -29,20 +23,16 @@
                 @change="onChangeAllItems"
                 ref="allChecked"
                 :checked="areAllItemsSelected"
-              >
+              />
             </th>
-            <th
-              :style="{width:'50px'}"
-              v-if="numberVisible"
-            >#</th>
+            <th :style="{ width: '50px' }" v-if="numberVisible">#</th>
             <th
               v-for="column in columns"
               :key="column.field"
-              :style="{width:column.width+'px'}"
+              :style="{ width: column.width + 'px' }"
             >
               <div class="gulu-table-header">
-
-                {{column.text}}
+                {{ column.text }}
                 <span
                   v-if="column.field in orderBy"
                   class="gulu-table-sorter"
@@ -50,86 +40,66 @@
                 >
                   <g-icon
                     name="asc"
-                    :class="{active:orderBy[column.field] === 'asc'}"
+                    :class="{ active: orderBy[column.field] === 'asc' }"
                   ></g-icon>
                   <g-icon
                     name="desc"
-                    :class="{active:orderBy[column.field] === 'desc'}"
+                    :class="{ active: orderBy[column.field] === 'desc' }"
                   ></g-icon>
                 </span>
               </div>
-
             </th>
-            <th
-              ref="actionsHeader"
-              v-if="$scopedSlots.default"
-            ></th>
+            <th ref="actionsHeader" v-if="$scopedSlots.default"></th>
           </tr>
         </thead>
         <tbody>
-          <template v-for="(item,index) in dataSource">
+          <template v-for="(item, index) in dataSource">
             <tr :key="item.id">
               <td
                 v-if="expendField"
-                :style="{width:'50px'}"
+                :style="{ width: '50px' }"
                 class="gulu-table-center"
               >
-                <g-icon
-                  name="right"
-                  @click="expendItem(item.id)"
-                ></g-icon>
+                <g-icon name="right" @click="expendItem(item.id)"></g-icon>
               </td>
               <td
                 v-if="checkable"
-                :style="{width:'50px'}"
+                :style="{ width: '50px' }"
                 class="gulu-table-center"
               >
                 <input
                   type="checkbox"
                   @change="onChangeItem(item, index, $event)"
                   :checked="inSelectedItems(item)"
-                >
+                />
               </td>
-              <td
-                v-if="numberVisible"
-                :style="{width:'50px'}"
-              >{{index+1}}
+              <td v-if="numberVisible" :style="{ width: '50px' }">
+                {{ index + 1 }}
               </td>
               <template v-for="column in columns">
-                <td
-                  :key="column.field"
-                  :style="{width:column.width+'px'}"
-                >
+                <td :key="column.field" :style="{ width: column.width + 'px' }">
                   <template v-if="column.render">
-                    <vnodes :vnodes="column.render({value:item[column.field]})"></vnodes>
+                    <vnodes
+                      :vnodes="column.render({ value: item[column.field] })"
+                    ></vnodes>
                   </template>
                   <template v-else>
-                    {{item[column.field]}}
+                    {{ item[column.field] }}
                   </template>
                 </td>
               </template>
 
               <!--编辑等操作-->
               <td v-if="$scopedSlots.default">
-                <div
-                  ref="actions"
-                  style="display:inline-block"
-                >
-                  <slot
-                    :item="item"
-                    frank="18"
-                  ></slot>
+                <div ref="actions" style="display:inline-block">
+                  <slot :item="item" frank="18"></slot>
                 </div>
-
               </td>
             </tr>
 
             <!--点击箭头展开的描述-->
-            <tr
-              v-if="inExpendedIds(item.id)"
-              :key="`${item.id}-expend`"
-            >
-              <td :colspan="columns.length+expendedCellColSpan">
+            <tr v-if="inExpendedIds(item.id)" :key="`${item.id}-expend`">
+              <td :colspan="columns.length + expendedCellColSpan">
                 {{ item[expendField] }}
               </td>
             </tr>
@@ -138,10 +108,7 @@
       </table>
     </div>
 
-    <div
-      class="gulu-table-loading"
-      v-if="loading"
-    >
+    <div class="gulu-table-loading" v-if="loading">
       <g-icon name="loading"></g-icon>
     </div>
   </div>
@@ -157,7 +124,6 @@ export default {
   },
   name: 'GuluTable',
   props: {
-
     height: {
       type: [Number, String]
     },
@@ -225,9 +191,11 @@ export default {
       return { text, field, width, render }
     })
 
-
     // 判断选中状态
-    if (this.selectedItems.length !== this.dataSource.length && this.selectedItems.length !== 0) {
+    if (
+      this.selectedItems.length !== this.dataSource.length &&
+      this.selectedItems.length !== 0
+    ) {
       this.$refs.allChecked.indeterminate = true
     }
 
@@ -244,46 +212,47 @@ export default {
     window.addEventListener('resize', () => this.onWindowResize)
     */
 
-    // 浅拷贝
-    let table2 = this.$refs.table.cloneNode(false)
-    // this.table2 = table2
-    table2.classList.add('gulu-table-copy')
+    this.$nextTick(() => {
+      // 浅拷贝
+      let table2 = this.$refs.table.cloneNode(false)
+      table2.classList.add('gulu-table-copy')
 
-    // 获取head的高度
-    //let tHead = this.$refs.table.children[0]
-    let tHead = this.$refs.table.children[0]
-    let { height } = tHead.getBoundingClientRect()
-    this.$refs.tableWrapper.style.marginTop = height + 'px'
-    this.$refs.tableWrapper.style.height = this.height - height + 'px'
-    table2.appendChild(tHead)
+      // 获取head的高度
+      let tHead = this.$refs.table.children[0]
+      let { height } = tHead.getBoundingClientRect()
+      this.$refs.tableWrapper.style.marginTop = height + 'px'
+      this.$refs.tableWrapper.style.height = this.height - height + 'px'
+      table2.appendChild(tHead)
 
-    this.$refs.wrapper.appendChild(table2)
-
+      this.$refs.wrapper.appendChild(table2)
+    })
 
     //这是没有scope的slots
     //console.log(this.$slots)
 
     // 计算自定义列slot的宽度
     if (this.$scopedSlots.default) {
-
       // 拿到第一个div的宽度,不要让div的宽度那么宽，display:inline-block
       let div = this.$refs.actions[0]
       let { width } = this.$refs.actions[0].getBoundingClientRect()
-      let parent = div.parentNode  //td
+      let parent = div.parentNode //td
       let styles = getComputedStyle(parent)
       // let pddingLeft = styles.getPropertyValue('padding-left')
       let pddingRight = styles.getPropertyValue('padding-right')
       let borderLeft = styles.getPropertyValue('border-left-width')
       let borderRight = styles.getPropertyValue('border-right-width')
-      let width2 = width + parseInt(pddingRight) + parseInt(pddingRight) + parseInt(borderLeft) + parseInt(borderRight) + 'px'
+      let width2 =
+        width +
+        parseInt(pddingRight) +
+        parseInt(pddingRight) +
+        parseInt(borderLeft) +
+        parseInt(borderRight) +
+        'px'
       this.$refs.actionsHeader.style.width = width2
       this.$refs.actions.map(div => {
         div.parentNode.style.width = width2
       })
     }
-
-
-
   },
   beforeDestroy() {
     // window.removeEventListener('resize', this.onWindowResize)
@@ -293,12 +262,15 @@ export default {
     areAllItemsSelected() {
       const a = this.dataSource.map(item => item.id).sort()
       const b = this.selectedItems.map(item => item.id).sort()
-      if (a.length !== b.length) { return false }
-      let equal = true
-      for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) {
-        equal = false
-        break
+      if (a.length !== b.length) {
+        return false
       }
+      let equal = true
+      for (let i = 0; i < a.length; i++)
+        if (a[i] !== b[i]) {
+          equal = false
+          break
+        }
       return equal
 
       // 使用选中的数据的长度和原数据的长度相等的做法并不准确有bug，需要让两个元素的id完全相等。
@@ -333,7 +305,7 @@ export default {
     // updateHeadersWidth(){
     //   let table2 = this.table2
     //   let tableHeader = Array.from(this.$refs.table.children).filter(node => node.tagName.toLowerCase() === 'thead')[0]
-    //   let tableHeader2 
+    //   let tableHeader2
     //   Array.from(table2.children).map(node => {
     //     if(node.tagName.toLowerCase() !== 'thead') {
     //       node.remove()
@@ -384,17 +356,15 @@ export default {
       } else {
         this.expendedIds.push(id)
       }
-
     },
     inExpendedIds(id) {
       return this.expendedIds.indexOf(id) >= 0
     }
   }
 }
-
 </script>
 <style lang="scss" scoped>
-@import "@/assets/css/var.scss";
+@import '@/assets/css/var.scss';
 $grey: darken($gray, 10%);
 .gulu-table {
   width: 100%;
